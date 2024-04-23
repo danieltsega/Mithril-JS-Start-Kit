b8c7556d1778079bdbd58a10fc3b2a4876725a5d
DannySanchez6658 <danieltsega6658@gmail.com>
2024-04-23 18:52:19 +0300

    [Feature]: Add the footer component to the main app and apply routing

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
 var m = require("mithril");
 var root = document.body;
+
+var NavBarComponent = require("../components/NavBar");
 var NewComp = require("../components/newComp");
 var Another = require("../components/anotherComp");
-var NavBarComponent = require("../components/NavBar");
+var FooterComponent = require("../components/FooterComp");
 
 var App = function () {
   return {
-    view: function () {
-      return m("body", [m(NavBarComponent), m(NewComp), m(Another)]);
+    view: function (vnode) {
+      return m("body", [
+        m(NavBarComponent),
+        m(vnode.attrs.content),
+        m(FooterComponent),
+      ]);
     },
   };
 };
 
-m.mount(root, App);
+m.route(root, "/", {
+  "/": {
+    render: function () {
+      return m(App, { content: NewComp });
+    },
+  },
+  "/another": {
+    render: function () {
+      return m(App, { content: Another });
+    },
+  },
+});
