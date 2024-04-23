6dd29055e865ef6de302d57f8b0f9bf12fad6d38
DannySanchez6658 <danieltsega6658@gmail.com>
2024-04-23 16:51:45 +0300

    [Feature]: Add a new button component

---
+++ components/ButtonComp.js
@@ -0,0 +1,15 @@
+var m = require("mithril");
+
+var Button = function () {
+  return {
+    view: function () {
+      return m(
+        "button",
+        { class: "bg-black text-white px-8 py-2 font-bold text-lg uppercase" },
+        "join"
+      );
+    },
+  };
+};
+
+module.exports = Button;
