8c4ea3f6a1846b5b26416cb52aec8a5bba709d0e
DannySanchez6658 <danieltsega6658@gmail.com>
2024-04-23 16:35:23 +0300

    [Feature]: Add the navbar component to the app

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ var m = require("mithril");
 var root = document.body;
 var NewComp = require("../components/newComp");
 var Another = require("../components/anotherComp");
+var NavBarComponent = require("../components/NavBar");
 
 var App = function () {
   return {
     view: function () {
-      return m("div", [m(NewComp), m(Another)]);
+      return m("body", [m(NavBarComponent), m(NewComp), m(Another)]);
     },
   };
 };
